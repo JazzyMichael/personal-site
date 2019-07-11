48e96728ea31360ca10a7fe2cf6387ddac377c50
Michael Mancini <thejazzymichael@gmail.com>
2019-07-11 19:00:10 -0400

    added plebeian deli project and updated seo tags

--- src/app/home/projects/projects.component.ts
+++ src/app/home/projects/projects.component.ts
@@ -8,21 +8,30 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
   projects: any[] = [
     {
+      title: 'Menu Mash',
+      subtitle: 'menumash.app',
+      logo: 'assets/Menu-Mash-Logo.jpg',
+      tech: 'Ionic, Capacitor, Angular, Firebase',
+      description: 'Swipe-to-order menu items from local restaurants (Tinder for food)',
+      website: 'https://menumash.app/',
+      repo: 'https://github.com/Jappzy/menumash'
+    },
+    {
       title: 'Story Stuff Saved',
       subtitle: 'storystuffsaved.com',
       logo: 'assets/Story-Stuff-Saved-Logo.jpg',
-      tech: 'PWA, Angular, Firebase',
+      tech: 'Angular PWA, Firebase',
       description: 'Selling used technology products',
       website: 'https://storystuffsaved.com/',
       repo: 'https://github.com/Jappzy/storystuffsaved.com'
     },
     {
-      title: 'Menu Mash',
-      subtitle: '*In Progress* menumash.app',
-      logo: 'assets/Menu-Mash-Logo.jpg',
-      tech: 'Ionic, Capacitor, Angular, Firebase, AdMob',
-      description: 'Swipe-to-order menu items from local restaurants',
-      website: 'https://menumash.app/',
+      title: 'Plebeian Deli',
+      subtitle: 'plebieandeli.art',
+      logo: 'assets/Plebeian-Deli-Logo.webp',
+      tech: 'Flutter, Angular, Firebase, Stripe',
+      description: 'Forum & blog for artists, e-commerce, online-magazine, free & payed subscriptions',
+      website: 'https://plebeiandeli.art/',
       repo: 'https://github.com/Jappzy/menumash'
     }
   ];
