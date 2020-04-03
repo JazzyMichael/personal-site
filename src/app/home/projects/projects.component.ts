bbd31f57c60f826d04a50075326dde5064fdc2ec
Michael Mancini <thejazzymichael@gmail.com>
2020-04-03 11:42:04 -0400

    added coronavirus projects and updated resume

--- src/app/home/projects/projects.component.ts
+++ src/app/home/projects/projects.component.ts
@@ -8,6 +8,24 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
   projects: any[] = [
     {
+      title: 'Authentic Essentials',
+      subtitle: 'authenticessentials.com',
+      logo: 'assets/Authentic-Essentials-Logo.png',
+      tech: 'Ionic, Capacitor, Angular, Firebase',
+      description: 'Anonymous posts about essential working & coronavirus experiences',
+      website: 'https://ionic-essentials.firebaseapp.com/tabs/home',
+      repo: 'https://github.com/Jappzy/essentials'
+    },
+    {
+      title: 'Carona Balona',
+      subtitle: 'caronabalona.com',
+      logo: 'assets/Carona-Balona-Logo.png',
+      tech: 'Svelte!',
+      description: 'The latest coronavirus data diplayed with cool charts',
+      website: 'https://caronabalona.com/',
+      repo: 'https://github.com/Jappzy/carona-balona'
+    },
+    {
       title: 'Menu Mash',
       subtitle: 'menumash.app',
       logo: 'assets/Menu-Mash-Logo.png',
@@ -17,6 +35,15 @@ export class ProjectsComponent {
       repo: 'https://github.com/Jappzy/menumash'
     },
     {
+      title: 'Plebeian Deli',
+      subtitle: 'plebieandeli.art',
+      logo: 'assets/Plebeian-Deli-Logo.png',
+      tech: 'Angular, Firebase, Stripe',
+      description: 'Social Network and marketplace for artists.',
+      website: 'https://plebeiandeli.art/',
+      repo: 'https://github.com/Jappzy/plebeian-deli'
+    },
+    {
       title: 'Story Stuff Saved',
       subtitle: 'storystuffsaved.com',
       logo: 'assets/Story-Stuff-Saved-Logo.png',
@@ -24,14 +51,6 @@ export class ProjectsComponent {
       description: 'Selling used technology products',
       website: 'https://storystuffsaved.com/',
       repo: 'https://github.com/Jappzy/storystuffsaved.com'
-    },
-    {
-      title: 'Plebeian Deli',
-      subtitle: 'plebieandeli.art',
-      logo: 'assets/Plebeian-Deli-Logo.png',
-      tech: 'Angular, Firebase, Stripe',
-      description: 'Network for artists and galleries, marketplace and subscription payments',
-      website: 'https://plebeiandeli.art/'
     }
   ];
 
